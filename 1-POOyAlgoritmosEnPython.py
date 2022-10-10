a14f3429006777d85de7ab5ed8518d564bf6eafa
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2022-10-10 09:24:17 -0500

    Starting POO with Python =D. Im so excited

---
+++ 1-POOyAlgoritmosEnPython.py
@@ -0,0 +1,8 @@
+'''
+    COURSE OBJETIVES
+    1 - Entender cómo funciona la Programación Orientada a Objectos.
+    2 - Entender cómo medir la eficiencia temporal y espacial de nuestros algoritmos.
+    3 - Entender cómo y por qué graficar.
+    4 - Aprender a resolver problemas de búsqueda, ordenación y optimización.
+
+'''
